caeb3190e117b7b461abfc8fe2445ea2f9dbff83
Ashika Singh <23JE0167@iitism.ac.in>
2023-12-18 22:39:47 +0530

    Final Commit

---
+++ app/src/main/java/jiya/learning/quizzify/MainActivity.kt
@@ -0,0 +1,11 @@
+package jiya.learning.quizzify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
